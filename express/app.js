9fb9c8c7f60d9541f50deed2328493d6fc7980a4
Volodymyr Pankiv <vpanki@softserveinc.com>
2020-05-01 17:01:32 +0300

    added ejs

--- express/app.js
+++ express/app.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
@@ -19,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: ''});
 });
 
 app.listen(3000);
