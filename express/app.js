13879e5b78815c0679642cc52d6eeadbde081600
Vova Pankiv <vovapankiw@gmail.com>
2020-06-30 09:52:37 +0300

    added multer to upload files

--- express/app.js
+++ express/app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const multer = require("multer");
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -17,9 +18,28 @@ const MONGODB_URI =
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: 'sessions',
+  collection: 'sessions'
 });
 const csrfProtection = csrf();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Math.random() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -28,18 +48,24 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
+  multer({
+    storage: fileStorage,
+    fileFilter: fileFilter
+  }).single('image')
 );
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use(
   session({
     secret: 'my secret',
     resave: false,
     saveUninitialized: false,
-    store: store,
+    store: store
   })
 );
 app.use(csrfProtection);
@@ -52,19 +78,19 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
-    .then((user) => {
+    .then(user => {
       if (!user) {
         return next();
       }
-
       req.user = user;
       next();
     })
-    .catch((err) => {
+    .catch(err => {
       next(new Error(err));
     });
 });
@@ -72,24 +98,26 @@ app.use((req, res, next) => {
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
-app.get('/500', errorController.get500)
+
+app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
 
-app.use((err, req, res, next) => {
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
   // res.redirect('/500');
   res.status(500).render('500', {
     pageTitle: 'Error!',
     path: '/500',
     isAuthenticated: req.session.isLoggedIn
   });
-})
+});
 
 mongoose
   .connect(MONGODB_URI)
-  .then((result) => {
+  .then(result => {
     app.listen(3000);
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
